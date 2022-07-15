adfa7fe569b8479679947407751ef846f22cb95e
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-07-14 22:07:00 -0500

    Stay method

--- js/blackjack.js
+++ js/blackjack.js
@@ -7,3 +7,42 @@ function askForCard() {
 		indiceCarta++;
 	}
 }
+
+function stay() {
+	document.getElementById("giveMe").disabled = true;
+	document.getElementById("stay").disabled = true;
+	document.getElementById("reset").style.visibility = "visible";
+	let pointsUser = 0;
+	let pointsCrupier = 0;
+	let info = document.getElementById("info");
+	// We count and print the player's points
+	for (i in cartasJugador) {
+		pointsUser += cartasJugador[i].valor;
+	}
+	// We draw cards to the crupier and count his points
+	while (pointsCrupier < 17) {
+		cartasCrupier.push(cartas[indiceCarta]);
+		pointsCrupier += cartas[indiceCarta].valor;
+		indiceCarta++;
+	}
+	// Points of the game are put in info
+	info.innerHTML = "Puntuación jugador: " + pointsUser + "<br>Puntuación crupier: " + pointsCrupier;
+	// We draw the crupier cards
+	carta.x = 50;
+	carta.y = 400;
+	for (i in cartasCrupier) {
+		dibujarCarta(cartasCrupier[i]);
+	}
+	// We check winner
+	if (pointsUser == 21) {
+		info.innerHTML +="<br><b>Blackjack!!! You WIn!!!</b>";
+	} else if (pointsUser > 21) {
+		info.innerHTML +="<br><b>You have lost... You have exceeded points</b>";
+	} else if (pointsCrupier > 21) {
+		info.innerHTML +="<br><b>You've won!!! The croupier has gone over points</b>";
+	} else if (pointsCrupier >= pointsUser) {
+		info.innerHTML += "<br><b>The croupier has won...</b>";
+	} else {
+		info.innerHTML += "<br><b>You've won!!!</b>";
+	}
+}
